a77d42852293d88ff114aaa982b7d63d06ec0d39
Matheus Motta <matheusmotta136@gmail.com>
2023-05-10 16:11:05 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  return data.employees.find((elementoA) => [elementoA.firstName,
+    elementoA.lastName].includes(employeeName));
 };
 
 module.exports = getEmployeeByName;
