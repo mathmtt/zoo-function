51a938db8ca1a39c6e07c4b07d901abaaef445d0
Matheus Motta <matheusmotta136@gmail.com>
2023-05-10 15:59:15 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  if (getAnimalsOlderThan) {
+    const ageAnimals = data.species.filter((elemento) => elemento.name === animal)
+      .every((elemento) => elemento.residents.map((elementoA) => elementoA.age)
+        .every((elementoB) => elementoB >= age));
+    return ageAnimals;
+  }
 };
 
 module.exports = getAnimalsOlderThan;
