883ad5eb06069ef9f05e9da02ec26e97cf5334af
Matheus Motta <matheusmotta136@gmail.com>
2023-05-10 15:40:20 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (ids) => {
   // seu código aqui
+  const array = [];
+  if (ids) {
+    const searchAnimal = data.species.filter((animale) => ids.includes(animale.id));
+    return searchAnimal;
+  }
+  return array;
 };
 
 module.exports = getSpeciesByIds;
