b39ccbddb44ee6087ce9a8139fca39526c411c77
Matheus Motta <matheusmotta136@gmail.com>
2023-05-11 16:16:39 -0300

    requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Primeiro Teste', () => {
+    const momento = handlerElephants('count');
+    const esperado = 4;
+    expect(momento).toEqual(esperado);
+  });
+  it('Segundo teste', () => {
+    const momento = handlerElephants('averageAge');
+    const esperado = 10.5;
+    expect(momento).toEqual(esperado);
+  });
+  it('Terceiro Teste', () => {
+    const momento = handlerElephants('names');
+    const esperado = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(momento).toEqual(esperado);
+  });
+});
