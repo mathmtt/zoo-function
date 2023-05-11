ff68aa6ab57f8c889a6834318cce1601f3951317
Matheus Motta <matheusmotta136@gmail.com>
2023-05-11 15:21:23 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,18 @@ const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
   // seu código aqui
+  if (!animal) {
+    return data.species.reduce(((acc, especie) => {
+      acc[`${especie.name}`] = especie.residents.length;
+      return acc;
+    }
+    ), {});
+  }
+  if (!animal.sex) {
+    return data.species.find((especie) => especie.name === animal.especie).residents.length;
+  }
+  return data.species.find((especie) => especie.name === animal.especie).residents
+    .filter((residentA) => residentA.sex === animal.sex).length;
 };
 
 module.exports = countAnimals;
