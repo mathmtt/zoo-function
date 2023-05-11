7518c6dfa733f7915454a65d4d023d22879781cb
Matheus Motta <matheusmotta136@gmail.com>
2023-05-10 22:24:05 -0300

    requisito 4 'o mais dificil ate agora, fiquei facil uns 30 minutos quebrando a cabeca'

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
+function isManager(id) {
   // seu código aqui
-};
+  if (isManager) {
+    return data.employees.some((element) => element.managers.includes(id));
+  }
+}
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
+  if (isManager(managerId)) {
+    const managers = [];
+    data.employees.forEach((elementoA) => {
+      if (elementoA.managers.includes(managerId)) {
+        managers.push(`${elementoA.firstName} ${elementoA.lastName}`);
+      }
+    });
+    return managers;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
